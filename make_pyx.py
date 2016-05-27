b0101db4b094102b1dc9204e0d51d9309def85f4
Tom Ritchford <tom@swirly.com>
2016-05-27 12:14:19 -0400

    Better accessors for enum types.

--- make_pyx.py
+++ make_pyx.py
@@ -91,9 +91,14 @@ ENUM_PROP_TEMPLATE = """\
     property {prop}:
         def __get__(self):
             return self.{TYPE}_NAMES[<int> self.{member_name}.{prop}]
-        def __set__(self, string x):
+        def __set__(self, object x):
             cdef uint8_t i
-            i = self.{TYPE}_NAMES.index(x)
+            if isinstance(x, str):
+                i = self.{TYPE}_NAMES.index(x)
+            else:
+                i = <uint8_t> x
+                if i >= len(self.{TYPE}_NAMES):
+                    raise ValueError("Can't understand value " + str(i))
             self.{member_name}.{prop} = <{Type}>(i)
 """
 
